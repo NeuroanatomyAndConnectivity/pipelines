38930f4a1280641b0bc6291b715f70bc0d9f68be
Aimi Watanabe <watanabe.aimi@gmail.com>
2013-08-19 17:01:29 +0200

    redo parse

--- src/clustering/parse.py
+++ src/clustering/parse.py
@@ -1,32 +1,29 @@
 import nibabel as nb
 import numpy as np
 import pickle
+import sys
 
-clusters = []
+inputFile = sys.argv[1]
 flag = 'found cluster!!'
 rowflag = 'rows=,['
-with open('/home/aimi/session1-0021002-lh.txt','rb') as openfile:
+
+clusters = []
+with open(inputFile,'rb') as openfile:
 	for line in openfile:
 		if line[:len(flag)] == flag:
 			clusters.append(line)
-vertices = np.zeros(155500)
-for index,cluster in enumerate(clusters):
-	words = cluster.split( )
-	for i,x in enumerate(words):
-		if x == rowflag:
-			while 1:
-				try: end = words[i+1][-2] == ']'
-				except IndexError: end = False
-				if end == False:
-					vertices[np.int(words[i+1])] = index
-					i+=1
-				if end == True:
-					vertices[np.int(words[i+1][:-2])] = index
-					break
-	
+
+for index, cluster in enumerate(clusters):
+    words = cluster.split( )
+    for x in enumerate(words):
+		if x[:len(rowflag)] == rowflag:
+            numbers = x[len(rowflag):-2].split(',')
+
+    savefile = '/SCR/Eran/newImage_cluster'+str(index)
+    with open(savefile,'wb') as openfile:
+	    pickle.dump(vertices, openfile)
+
 #newImage = nb.minc.MincImage(vertices, None)
-savefile = '/home/aimi/newImage'
 #nb.save(newImage, savefile)
 
-with open(savefile,'wb') as openfile:
-	pickle.dump(vertices, openfile)
+
