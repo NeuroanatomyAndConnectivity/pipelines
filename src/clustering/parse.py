b3eaf888e54386a557cbb0c69537f27de4c1041d
Aimi Watanabe <watanabe.aimi@gmail.com>
2013-08-20 16:18:41 +0200

    better parsing for all log files

--- src/clustering/parse.py
+++ src/clustering/parse.py
@@ -2,10 +2,23 @@ import nibabel as nb
 import numpy as np
 import pickle
 import sys
+import surfer.io as surf
+from variables import freesurferdir
 
 inputFile = sys.argv[1]
 flag = 'found cluster!!'
 rowflag = 'rows=,['
+hemi = 'lh'
+
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
+[vertices,colortable,names] = surf.read_annot(freesurferdir+'/fsaverage4/label/'+hemi[-2:]+'.aparc.a2009s.annot', orig_ids=True)
+surface = np.zeros_like(vertices)
 
 clusters = []
 with open(inputFile,'rb') as openfile:
@@ -14,16 +27,18 @@ with open(inputFile,'rb') as openfile:
 			clusters.append(line)
 
 for index, cluster in enumerate(clusters):
-    words = cluster.split( )
-    for x in enumerate(words):
-		if x[:len(rowflag)] == rowflag:
-            numbers = x[len(rowflag):-2].split(',')
-
-    savefile = '/SCR/Eran/newImage_cluster'+str(index)
-    with open(savefile,'wb') as openfile:
-	    pickle.dump(vertices, openfile)
-
-#newImage = nb.minc.MincImage(vertices, None)
-#nb.save(newImage, savefile)
+    startIndex = cluster.rfind(rowflag)
+    stopIndex = cluster.rfind(']')-1
+    for x in cluster[startIndex:stopIndex].split(','):
+        vertex = x.lstrip()
+        if vertex.isdigit():
+            surface[int(x)] = 1
+        if vertex.endswith(']'):
+            surface[int(x[:-1])] = 1
+    savefile = inputFile.split('.')[0]+'_cluster'+str(index)
+    newImage = nb.nifti1.Nifti1Image(surface, None)
+    nb.save(newImage, savefile)
+    #with open(savefile,'wb') as openfile:
+    #pickle.dump(numbers, openfile)
 
 
